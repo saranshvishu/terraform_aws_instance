ff893a595a9e9a601bc13cab0fa316ec2efa40e6
Saransh <91221990+saranshvishu@users.noreply.github.com>
2025-03-26 21:22:26 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "one" {
+  count         = 2
+  ami           = "ami-0866a3c8686eaeeba"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "raham"
+  }
+}
